c8a7b03b9b72deef232e8f79d7415bd955b198ab
aurorex <selva_beat@hotmail.com>
2018-04-27 23:58:51 -0500

    agregando maquetado inicial

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  // SLIDER 
+  $('#slider').slick({
+    // normal options...
+      infinite: false,
+      // the magic
+      responsive: [{
+      
+          breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              infinite: true
+            }
+        }, 
+        {
+          breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+            // dots: true
+            }
+    
+          }, 
+        {
+          breakpoint: 300,
+        // settings: "unslick" // destroys slick
+      }]
+    });
+  
+});
